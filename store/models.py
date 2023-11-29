2ca84aecc989b2408fb299057c11513331878565
Forrest Goryl <forrestgoryl@outlook.com>
2023-11-29 10:30:27 -0600

    Create additional models in store/models.py.

--- store/models.py
+++ store/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+
+class Collection(models.Model):
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='featured_product')
+    title = models.CharField(max_length=255)
+
+
 class Product(models.Model):
+    promotion = models.ManyToManyField(Promotion)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -34,6 +47,13 @@ class Customer(models.Model):
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 
+class Address(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+
+
 class Order(models.Model):
     PENDING_STATUS = 'P'
     COMPLETE_STATUS = 'C'
@@ -45,5 +65,23 @@ class Order(models.Model):
         (FAILED_STATUS, 'Failed')
     ]
 
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=PENDING_STATUS)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
